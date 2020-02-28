9364976c04cc2a94f95b3a60783d236399f92ae5
Dawson <djtorch123@gmail.com>
2020-02-28 16:15:24 -0500

    test program to run

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ Dawson Tocarchick, Nicholas Riggins, Kyle Limbaga
 Main.py for Speech to text analysis
 """
 
-Dad this is a test to see if it uploads to the board.
+import Adafruit_BBIO.GPIO as GPIO
+
+outpin = "P9_12"
+GPIO.setup(outpin,GPIO.OUT)
+from time import sleep
+
+for i in range(0,5):
+    GPIO.output(outpin,GPIO.HIGH)
+    print("You Suck")
+    sleep(1)
+    GPIO.output(outpin,GPIO.LOW)
+    print("You rock")
+    sleep(1)
+GPIO.cleanup()
