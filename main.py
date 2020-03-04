ec03e90ba5de18e4c7693c7fbe31b51f12a872a5
Dawson <djtorch123@gmail.com>
2020-03-04 15:31:06 -0500

    once again.

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ voltage = value * 1.8
 loop = True
 
 while loop:
-    print(' Value = %(val) \n Voltage = %(volt)' %('val': value, 'volt': voltage))
+    print('Value = %(s)\nVoltage = %(s)' %(value,voltage))
     sleep(.5)
     
