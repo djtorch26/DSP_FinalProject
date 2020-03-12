f1e59c13e7397679ecfda7b7290dce6df9b55394
dawsontorch <tocarchid5@students.rowan.edu>
2020-03-11 22:26:44 -0400

    ADC1.7
    Added Input functionality

--- Setup/pinSetup.py
+++ Setup/pinSetup.py
@@ -4,9 +4,14 @@ This module instantiates pins to be used during speech analysis
 import Adafruit_BBIO.ADC as ADC
 output = []
 
+
+def SampleTime():
+    Userinput = input('Enter Record Time in seconds')
+    return Userinput
 def StartSample(pin):
+    t = SampleTime() * 666
     ADC.setup()
-    for i in range(0, 8500): #should be 
+    for i in range(0, t): #should be 
         output.append(ADC.read(pin)*1.8)
         print(output[i])
     return output
