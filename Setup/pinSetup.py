335a2081d15eb13ffbec93c2cb2a2a5b4c48c493
dawsontorch <tocarchid5@students.rowan.edu>
2020-03-11 21:51:36 -0400

    ADC1.4
    decrease to 1000 samples

--- Setup/pinSetup.py
+++ Setup/pinSetup.py
@@ -6,6 +6,6 @@ output = []
 
 def StartSample(pin):
     ADC.setup()
-    for i in range(0, 100000):
+    for i in range(0, 1000):
         output.append(ADC.read_raw(pin))
         print(output[i])
