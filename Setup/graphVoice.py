c4161033b32c1ae14ead062dfdc205b0832a9844
dawsontorch <tocarchid5@students.rowan.edu>
2020-04-23 13:43:31 -0400

    email function added

--- Setup/graphVoice.py
+++ Setup/graphVoice.py
@@ -9,12 +9,11 @@ import matplotlib as mpl
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using non-interactive Agg backend')
     mpl.use('Agg')
+    
 import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import sys
-
-
 
 spf = wave.open("test.wav", "r")
 
--- Setup/sendemail.py
+++ Setup/sendemail.py
@@ -11,38 +11,43 @@ import smtplib
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
+from email.mime.audio import MIMEAudio
 from email.mime.text import MIMEText
 
 
 gmail_user = "dtemail098@gmail.com"
 gmail_pwd = "gmailAccount1"
 to = "djtorch123@gmail.com"
-subject = "Wave File Test"
-text = "Hey this is a test to see if it emails wav files."
-attach = 'test.wav'
 
-msg = MIMEMultipart()
-msg['From'] = gmail_user
-msg['To'] = to
-msg['Subject'] = subject
-msg.attach(MIMEText(text))
+def emailFile(file):
+    
+    attach = file 
+    if "test" in file:
+        subject = "Wave File"
+        text = "This is the recorded voice from the Microphone. /n To use this file you nust add the extension .wav to the no name file."
+        
+        msg = MIMEMultipart()
+        msg['From'] = gmail_user
+        msg['To'] = to
+        msg['Subject'] = subject
+        msg.attach(MIMEText(text))
+        
+        part = MIMEAudio('audio', 'octet-stream')
+        part.set_payload(open(attach, 'rb').read())
+        
+        encoders.encode_base64(part)
+        part.add_header('Content-Dispostion', 'attachment; filename=%s"' % os.path.basename(attach))
+        
+        msg.attach(part)
+        mailServer = smtplib.SMTP("smtp.gmail.com",587)
+        mailServer.ehlo()
+        mailServer.starttls()
+        mailServer.ehlo()
+        mailServer.login(gmail_user, gmail_pwd)
+        mailServer.sendmail(gmail_user, to, msg.as_string())
+        mailServer.close()
+        print("Email Sent!")
 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(open(attach, 'rb').read())
-
-encoders.encode_base64(part)
-part.add_header('Content-Dispostion', 'attachment; filename=%s"' % os.path.basename(attach))
-
-msg.attach(part)
-mailServer = smtplib.SMTP("smtp.gmail.com",587)
-mailServer.ehlo()
-mailServer.starttls()
-mailServer.ehlo()
-mailServer.login(gmail_user, gmail_pwd)
-mailServer.sendmail(gmail_user, to, msg.as_string())
-mailServer.close()
-print("Email Sent!")
-
-
+emailFile('test.wav')
 
 
