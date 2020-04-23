e44eb3b10c0e4e6ea03b9d04116e687de5ebfcce
dawsontorch <tocarchid5@students.rowan.edu>
2020-04-23 01:41:30 -0400

    encoder change again

--- Setup/sendemail.py
+++ Setup/sendemail.py
@@ -22,7 +22,7 @@ import email
 #from email.mime.multipart import MIMEMultipart
 #from email.mime.text import MIMEText
 
-from email.encoders import encoders
+from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
